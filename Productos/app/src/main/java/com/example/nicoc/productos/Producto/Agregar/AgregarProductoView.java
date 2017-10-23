753058400c43c23fe92b7e93f561b3f404c7bb77
nicoc <norfuen@hotmail.com>
2017-10-23 00:22:51 -0300

    En agregar producto imagen guardada en un archivo

--- Productos/app/src/main/java/com/example/nicoc/productos/Producto/Agregar/AgregarProductoView.java
+++ Productos/app/src/main/java/com/example/nicoc/productos/Producto/Agregar/AgregarProductoView.java
@@ -1,9 +1,16 @@
 package com.example.nicoc.productos.Producto.Agregar;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
@@ -12,6 +19,14 @@ import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 import com.example.nicoc.productos.Producto.Listado.ListadoView;
 import com.example.nicoc.productos.R;
 import com.google.common.collect.Range;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,6 +44,9 @@ public class AgregarProductoView extends AppCompatActivity implements IProducto.
     @BindView(R.id.txtPrecio) EditText txtPrecio;
     @BindView(R.id.txtStock) EditText txtStock;
     @BindView(R.id.txtImagen) EditText txtImagen;
+    @BindView(R.id.imagenProducto)
+    ImageView imagenProducto;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +74,13 @@ public class AgregarProductoView extends AppCompatActivity implements IProducto.
         validator.addValidation(txtStock, Range.greaterThan(-1), "Stock 0 o mayor");
     }
 
+    @OnClick(R.id.txtImagen) void seleccionarImagen(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+
+        //intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+        startActivityForResult(intent, 1);
+    }
     @OnClick(R.id.btnAgregarProducto)@Override public void agregarProducto(){
         this.validator.clear();
         if (!this.validator.validate())
@@ -64,13 +89,92 @@ public class AgregarProductoView extends AppCompatActivity implements IProducto.
         String codigo = txtCodigo.getText().toString();
         String nombre = txtNombre.getText().toString();
         String descripcion = txtDescripcion.getText().toString();
-        String imagen = txtImagen.getText().toString();
+        //String imagen = txtImagen.getText().toString();
         Double precio = Double.valueOf(txtPrecio.getText().toString());
         Integer stock = Integer.valueOf(txtStock.getText().toString());
 
+        String imagen = saveImage();
         this.presenter.agregarProducto(
                 codigo, nombre, descripcion, precio, stock, imagen
         );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bundle extras = data.getExtras();
+                Bitmap bMap = (Bitmap) extras.get("data");
+                imagenProducto.setImageBitmap(bMap);
+                //saveImage(bMap);
+                /*
+                File f = new File(Environment.getExternalStorageDirectory().|());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    Bitmap bitmap;
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+                    imagenProducto.setImageBitmap(bitmap);
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "Phoenix" + File.separator + "default";
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    try {
+                        outFile = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                */
+            }
+        }
+    }
+
+    public String saveImage(){
+        FileOutputStream fileOutputStream = null;
+        File file = getDisc();
+        if (!file.exists() && !file.mkdir()){
+            mostrarError("No se puede abrir archivo para escribir");
+            return "";
+        }
+        SimpleDateFormat format = new SimpleDateFormat("yyyymmsshhmmss");
+        String date = format.format(new Date());
+        String name = "img"+date+".jpg";
+        String file_name = file.getAbsolutePath()+File.separator+name;
+        File new_file = new File(file_name);
+        try {
+            fileOutputStream = new FileOutputStream(new_file);
+            Bitmap bmap= ((BitmapDrawable)imagenProducto.getDrawable()).getBitmap();
+            bmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "";
+        }
+        return name;
+    }
+
+    public File getDisc(){
+        File file = android.os.Environment.getExternalStorageDirectory();
+        return new File(file, "imagenes");
     }
 
     @Override
